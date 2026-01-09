01afc4267093527096d8a9e526c6dc13a689bb84
Shreyash-chavare <shreyashchavare04@gmail.com>
2026-01-09 10:21:39 +0530

    updated

--- Backend/app.js
+++ Backend/app.js
@@ -33,7 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieparser());
 app.use(cors({
-    origin: ["http://localhost:5173"]  ,
+    origin: ["https://collaborative-code-editor-ilbd.onrender.com"] ,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Accept', 'Authorization']
